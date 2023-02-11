1759bb4060a9b5dc9f292fe06cc2a4fc0f9393a0
Isabel Fernandez <if0646a@student.american.edu>
2023-02-11 16:37:51 -0500

    started coding

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Programming Assignment 1: The Suffix Array
 Isabel Fernandez
 CMSC 701 Spring 23'
-February 28th, 2023ß
+February 28th, 2023
+
+## Preliminary Infomration
+* **Coding Language:** C++
+* **Genome Refrence:** [Human Genome Resources at NCBI (Refrence Genome Sequence GRCh38)](https://www.ncbi.nlm.nih.gov/projects/genome/guide/human/index.shtml)
---
+++ main_code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Function to Build Suffix Array
+void buildsa(int preftab=0, string reference, string output) {
+    // Read FASTA file
+    
+    // Build Suffix Array
+    
+    // Build Seconday Index (if requested)
+    
+    // Write String and Suffix Array to Binary File
+    
+    return 0
+}
+
+int main() {
+
+  return 0;
+}
